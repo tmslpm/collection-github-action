4d0ecbf066dda8413f04958e870c9619401bc752
Thomas.L <144795071+tmslpm@users.noreply.github.com>
2023-10-02 17:32:43 +0000

    ...

--- generateReadme.sh
+++ generateReadme.sh
@@ -8,19 +8,27 @@ BACK_TO_TOP="<pre align=center>↑↑↑ <a href='#-collection-github-action' ti
 #--------------------------------------------------------------
 # get all workflows in repos
 RESULT=$(ls $ENTRY_PATH)
-
+ 
 #--------------------------------------------------------------
+# create list table link
 # append all action title + link in variable 
 strList=""
+strTableList="<ul>"
 for f in $RESULT; do
-    title=$(cat "${ENTRY_PATH}/${f}" | grep "^name:")
-    description=$(cat "${ENTRY_PATH}/${f}" | grep "# description:")
-    strList="${strList}<h4>${title/name:/''}</h4><p>${description/\# description:/''}</p><p>↳ 🔗 <a href='${URL}${f}' title='open the action'>${f}</a> (<a href='${RAW_URL}${f}' title='open the action'>raw</a>)</p><hr>${BACK_TO_TOP}"
+    title=$(cat "${ENTRY_PATH}/${f}" | grep "^# name:")
+    description=$(cat "${ENTRY_PATH}/${f}" | grep "^# description:")
+    # gen html title/description/link to action
+    strList="${strList}<hr><h4>${title/\# name:/''}</h4><p>${description/\# description:/''}</p><p>↳ 🔗 <a href='${URL}${f}' title='open the action'>${f}</a> (<a href='${RAW_URL}${f}' title='open the action'>raw</a>)</p>${BACK_TO_TOP}"
+    # gen html header table
+    strTableList="${strTableList}<li><a>${title/' '/'-'}</a></li>"
 done
+strTableList="${strTableList}</ul>"
 
 #--------------------------------------------------------------
 # write in readme
 cat > README.md << EOL
-<h1>🚀 Collection Github Action</h1>
-${strList}
+    <h1>🚀 Collection Github Action</h1>
+    <h2>Table</h2>
+    ${strTableList}
+    ${strList}
 EOL
